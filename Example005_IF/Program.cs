84c53b5ec3108017e335161189dbb69560af7b30
Tyamenko Vyacheslav <slavo90@mail.ru>
2022-08-14 19:43:35 +0700

    Add Example005 IF

---
+++ Example005_IF/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "slava")
+{
+    Console.WriteLine("Добро пожаловать повелитель");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.Write(username);
+}
