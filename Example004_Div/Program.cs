99999f9d1e147066bd557a3396e0d95345e70227
Tyamenko Vyacheslav <slavo90@mail.ru>
2022-08-14 19:12:15 +0700

    Add Example004 Sum

--- Example004_Div/Program.cs
+++ Example004_Div/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int a = new Random().Next(0, 10);  //1,2,3...9
+Console.WriteLine(a);
+int b = new Random().Next(0, 10);
+Console.WriteLine(b);
+int result = a + b;
+Console.WriteLine ( result);
