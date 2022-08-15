db7123ffda0aa6e4e521090a85f743909dfbcf41
Tyamenko Vyacheslav <slavo90@mail.ru>
2022-08-15 23:41:11 +0700

    Add Seminar001

---
+++ Seminar/Seminar001/Program.cs
@@ -0,0 +1,9 @@
+﻿/*
+Написать программу которая на вход принимает число и возводит его в квадрат
+*/
+int userNumber = new int();
+Console.WriteLine("Введите число: ");
+userNumber = Convert.ToInt32(Console.ReadLine());
+int result = (userNumber*userNumber);
+//Console.WriteLine("Степень числа " +userNumber+ " = "  +result);
+Console.WriteLine($"Степень числа {userNumber} = {result}");
