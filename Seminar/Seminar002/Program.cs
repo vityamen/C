a95d47c374f8873f27c3d79dd75814e70a1b9e78
Tyamenko Vyacheslav <slavo90@mail.ru>
2022-08-16 00:11:40 +0700

    добавил задачу №1

---
+++ Seminar/Seminar002/Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+Написать программу проверяющую является ли второе число квадратом первого
+*/
+int userNumber1 = new int();
+Console.Write("Введите число 1: ");
+userNumber1 = Convert.ToInt32(Console.ReadLine());
+
+int userNumber2 = new int();
+Console.Write("Введите число 2: ");
+userNumber2 = Convert.ToInt32(Console.ReadLine());
+
+int result = (userNumber1*userNumber1);
+
+if (result == userNumber2){
+    Console.WriteLine("Да");
+    Console.WriteLine($"Степень числа {userNumber1} = {userNumber2}");
+    }
+else{
+    Console.WriteLine("Нет");
+}
+
