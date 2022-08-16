7ae19d4be109cc88cc3058c60828a83223f65b14
Tyamenko Vyacheslav <slavo90@mail.ru>
2022-08-17 00:37:46 +0700

    Add all files

---
+++ Seminar/Seminar004/Program.cs
@@ -0,0 +1,13 @@
+﻿/*
+Напишите программу, которая будет на вход принимает одно число (N),
+а на выходе показывает все целые числа в промежутке от -N до N.
+4 -> "-4, -3, -2, -1, 0, 1, 2, 3, 4" 2 -> " -2, -1, 0, 1, 2"
+*/
+int Number = new int();
+int i=0;
+Console.Write("Введите число: ");
+Number = Convert.ToInt32(Console.ReadLine());
+for (i= -Number; i<=Number; i++){
+    Console.WriteLine(i);
+}
+
---
+++ Seminar/Seminar005/Program.cs
@@ -0,0 +1,11 @@
+﻿/*
+Напишите программу, которая принимает на вход трёхзначное число
+и на выходе показывает последнюю цифру этого числа.
+456 -> 6 782 -> 2 918 -> 8
+*/
+int Number = new int();
+int Number2 = new int();
+Console.Write("Введите трехзначное число: ");
+Number = Convert.ToInt32(Console.ReadLine());
+Number2 = Number%10;
+    Console.WriteLine(Number2);
