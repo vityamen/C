6ff229e8923b5983c8e91b23558aa998f49322b8
Tyamenko Vyacheslav <slavo90@mail.ru>
2022-08-19 23:54:25 +0700

    Add function

---
+++ Examples008/Program.cs
@@ -0,0 +1,26 @@
+﻿int Max(int arg1, int arg2, int arg3){
+    int result = arg1;
+    if(arg2 > result)  result = arg2;
+    if(arg3 > result)  result = arg3;
+    return result;
+}
+
+int a1 = 21;
+int b1 = 1;
+int c1 = 66;
+int a2 = 6;
+int b2 = 5;
+int c2 = 56;
+int a3 = 12;
+int b3 = 10;
+int c3 = 100;
+
+//int max1 = Max(a1, b1, c1);
+//int max2 = Max(a2, b2, c2);
+//int max3 = Max(a3, b3, c3);
+int max = Max(
+    Max (a1, b1, c1),
+    Max (a2, b2, c2),
+    Max (a3, b3, c3));
+//int max = Max(max1, max2, max3);
+Console.WriteLine("Максимальное число: " +max);
