ab28b0f0771baf30b76521d06e5e8b339aba7a4b
Tyamenko Vyacheslav <slavo90@mail.ru>
2022-08-22 01:01:07 +0700

    Add task2

---
+++ Seminar2/Seminar002/Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+Напишите программу, которая выводит случайное трёхзначное число
+и удаляет вторую цифру этого числа. 456 -> 46 782 -> 72 918 -> 98
+*/
+int GetNumRandinRange(int MinNum, int MaxNum)
+{
+    int results = new Random().Next(MinNum, MaxNum+1);
+    return results;
+}
+
+int GetSecondDigitFromNumber(int number)
+{
+    string first = Convert.ToString (number / 100);
+    string second = Convert.ToString (number % 10);
+    string resultStr = first+second;
+    int resultInt = Convert.ToInt32(resultStr);
+    return resultInt;
+}
+
+
+int RandNum = GetNumRandinRange(100, 999);
+int SecondDigit = GetSecondDigitFromNumber(RandNum);
+Console.WriteLine($"{RandNum} : {SecondDigit}");
