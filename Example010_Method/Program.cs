29ad636c1756b3f764fe61cb9ff60bf816b38a5a
Tyamenko Vyacheslav <slavo90@mail.ru>
2022-08-21 17:46:46 +0700

    Add method

---
+++ Example010_Method/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = {12,210,36,44,53,210,71,18,29};
+
+int n = array.Length;
+int find = 210;
+
+int index = 0;
+
+while (index < n){
+
+    if(array[index]== find){
+      Console.WriteLine(index); 
+      break; 
+    }
+    index++;
+
+} 
