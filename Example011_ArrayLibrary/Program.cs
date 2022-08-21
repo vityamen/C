70140b1b31ae9a2121b0fca10a70c2eeaabea81e
Tyamenko Vyacheslav <slavo90@mail.ru>
2022-08-21 18:37:08 +0700

    Add Void Function

---
+++ Example011_ArrayLibrary/Program.cs
@@ -0,0 +1,24 @@
+﻿void FillArray(int[] collection){
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+    collection[index] = new Random().Next(0,10);
+    index++; 
+    }
+}
+
+void FillPrint(int[] coll){
+    int count = coll.Length;
+    int position = 0;
+    while (position < count)
+    {
+    Console.WriteLine(coll[position]);
+    position++; 
+    }
+}
+
+int[] array = new int[10];
+
+FillArray(array);
+FillPrint(array);
