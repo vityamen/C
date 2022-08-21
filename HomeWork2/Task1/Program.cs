ebb3f757954f657445a2605c3037f664f1e8302b
Tyamenko Vyacheslav <slavo90@mail.ru>
2022-08-22 01:27:10 +0700

    Сделал первое задание из ДЗ 2

---
+++ HomeWork2/Task1/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ Напишите программу, которая принимает на вход трёхзначное число
+ и на выходе показывает вторую цифру этого числа.
+
+456 -> 5
+782 -> 8
+918 -> 1
+*/
+int GetNumRandinRange(int MinNum, int MaxNum)
+{
+    int results = new Random().Next(MinNum, MaxNum+1);
+    return results;
+}
+
+int GetSecondDigitFromNumber(int number)
+{
+    int first = number % 100;
+    int result = first / 10;
+    return result;
+}
+
+
+int RandNum = GetNumRandinRange(100, 999);
+int SecondDigit = GetSecondDigitFromNumber(RandNum);
+Console.WriteLine($"{RandNum} : {SecondDigit}");
