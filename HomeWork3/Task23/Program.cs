43596e0ff46ef9d0a5f8075e50cba9e043db8ffe
Tyamenko Vyacheslav <slavo90@mail.ru>
2022-08-28 00:21:31 +0700

    сделал третье задание 3 семинара

---
+++ HomeWork3/Task23/Program.cs
@@ -0,0 +1,16 @@
+﻿/*Задача 23
+Напишите программу, которая принимает на вход число (N)
+и выдаёт таблицу кубов чисел от 1 до N.
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125
+*/
+int Num = 0;
+int i = 0;
+Console.WriteLine("Введите число N");
+Num = Convert.ToInt32(Console.ReadLine());
+for (i = 1; i <= Num; i++)
+{   
+    
+    double NewNum = Math.Pow(i,3);
+    Console.Write($"{NewNum}, ");
+}
