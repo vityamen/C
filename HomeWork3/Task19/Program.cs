4941083951c40112198bd8e698db15f8f70afde1
Tyamenko Vyacheslav <slavo90@mail.ru>
2022-08-27 23:18:57 +0700

    Сделал перовое задание к 3 Семинару

---
+++ HomeWork3/Task19/Program.cs
@@ -0,0 +1,38 @@
+﻿/*Задача 19
+Напишите программу, которая принимает на вход пятизначное число и проверяет,
+является ли оно палиндромом.
+14212 -> нет
+12821 -> да
+23432 -> да
+*/
+string CheckThePolindrom(int N)
+{
+    string result = " ";
+    int N1 = N/10000;
+    int N2 = N/1000%10;
+    int N3 = N/100%10;
+    int N4 = N%100/10;
+    int N5 = N%10;
+    if ((N1 == N5) && (N2 == N4))
+    {
+        Console.WriteLine($"{N1}, {N2}, {N3}, {N4}, {N5}");
+        result = "является полиндромом";
+        return result;
+    }
+
+    else 
+    {
+        Console.WriteLine($"{N1}, {N2}, {N3}, {N4}, {N5}");
+        result = "не является полиндромом";
+        return result;
+    }
+
+
+}
+
+int N = 0;
+Console.WriteLine("Введите пятизначное число: ");
+N = Convert.ToInt32(Console.ReadLine()); 
+Console.WriteLine("Ошибка ввода ");
+string Pol = CheckThePolindrom(N);
+Console.WriteLine($"Введенное число: {N} {Pol} ");
---
+++ Seminar3/Seminar002/Program.cs
@@ -0,0 +1,41 @@
+﻿/*
+Напишите программу, которая по заданному номеру четверти,
+показывает диапазон возможных координат точек в этой четверти (x и y)
+*/
+
+int GetCoordinateFromQuoter(int quoter)
+{   
+    int result = 0;
+    if (x > 0 && y > 0 )
+    {
+        result = 1;
+    }
+    else if (x < 0 && y > 0)
+    {
+        result = 2;
+    }
+    else if (x < 0 && y < 0)
+    {
+        result = 3;
+    }
+    else if (x > 0 && y < 0)
+    {
+        result = 4;
+    }
+    else
+    {
+       Console.ForegroundColor = ConsoleColor.Red;
+       Console.WriteLine($"X и У не должны быть равны 0 вы ввели {x} {y}");
+       Console.ResetColor();
+    }
+    return result;
+}
+int userX = 0;
+int userY = 0;
+Console.WriteLine("Введите X: ");
+userX = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите Y: ");
+userY = Convert.ToInt32(Console.ReadLine());
+
+int quoter = GetQuoterFromCoordinate(userX, userY);
+Console.WriteLine($"Координаты {userX}:{userY} находятся в {quoter} четверти");
