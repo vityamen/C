17eadebfd869d8906d72bb26283a56d82be12b02
Tyamenko Vyacheslav <slavo90@mail.ru>
2022-08-30 23:47:29 +0700

    Add Example016 Fibonachi

---
+++ Example016 fibonachi/Program.cs
@@ -0,0 +1,9 @@
+﻿double Fibonachi(int n)
+{
+    if(n == 1 || n == 2) return 1;
+    else return Fibonachi(n-1)+Fibonachi(n-2);
+}
+for(int i = 1; i < 41; i++)
+{
+    Console.WriteLine($"Фибоначчи числа {i}! = {Fibonachi(i)}");
+}
