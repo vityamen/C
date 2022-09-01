6a600befd7a8cfa69b06d3e46e7569cecc99a951
Tyamenko Vyacheslav <slavo90@mail.ru>
2022-09-01 22:58:32 +0700

    Add task24 4seminar

---
+++ HomeWork4/Task25/Program.cs
@@ -0,0 +1,9 @@
+﻿void Pow(int a, int b)
+{   
+    for(int i = 1; i <= 2; i++)
+    {
+        Console.WriteLine($"Введите число{i}");
+        int[,] = new int Console.ReadLine();
+
+    }
+}
---
+++ Seminar4/Task24/Program.cs
@@ -0,0 +1,19 @@
+﻿/*
+Принимает на вход число А и выводит сумму чисел от 1 до А
+*/
+Console.Clear();
+Console.WriteLine("Введите число А");
+int Num = int.Parse(Console.ReadLine());
+
+int GetSumNumbers(int number)
+{
+    int sum = 0;
+    int i = 1;
+    while(i <= number)
+    {
+        sum +=i;
+        i++;
+    }
+    return sum;
+}
+Console.WriteLine($"Сумма чисел от 1 до А={GetSumNumbers(Num)}");
