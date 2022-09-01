d2988aa9ecee97c5f0de960e418788a392986e75
Tyamenko Vyacheslav <slavo90@mail.ru>
2022-09-02 01:13:25 +0700

    Сделал 27 задачу из ДЗ

--- HomeWork4/Task25/Program.cs
+++ HomeWork4/Task25/Program.cs
@@ -1,9 +1,24 @@
-﻿void Pow(int a, int b)
-{   
-    for(int i = 1; i <= 2; i++)
-    {
-        Console.WriteLine($"Введите число{i}");
-        int[,] = new int Console.ReadLine();
+﻿/*
+Напишите цикл, который принимает на вход два числа (A и B) 
+и возводит число A в натуральную степень B.
+*/
 
+Console.Clear();
+Console.WriteLine($"Введите число A= ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Введите число B= ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Числом {num1} в степени {num2} является {PowNum(num1,num2)}");
+
+double PowNum(int a, int b)
+{  
+    int i = 0;
+    double degree = 1;
+    while(i < b)
+
+    {           
+        degree =degree*a;
+        i++;
     }
+    return degree;
 }
---
+++ HomeWork4/Task27/Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+Задача 27: Напишите программу, которая принимает на вход число
+и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12
+*/
+
+Console.Write($"Введите число N= ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(SumAllDigit(num1));
+
+int SumAllDigit(int number)
+{   
+    int digit = 0;
+    int sum = 0;
+    while( number != 0)
+    {
+        digit = number%10;
+        number = number/10;
+        sum = sum + digit;
+        Console.Write($"{digit}, ");
+    }
+    Console.WriteLine();
+    return sum;
+}
