c8dfb4f1dc50c5c88db0654342652e38b2b87ba6
Tyamenko Vyacheslav <slavo90@mail.ru>
2022-09-02 01:49:29 +0700

    Сделал задание 29 ДЗ 4 семинар

---
+++ HomeWork4/Task29/Program.cs
@@ -0,0 +1,23 @@
+﻿void FillArray(int[] collection){
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+    collection[index] = new Random().Next(0,9);
+    index++; 
+    }
+}
+
+void FillPrint(int[] coll){
+    int count = coll.Length;
+    int position = 0;
+    while (position < count)
+    {
+    Console.Write($"{coll[position]}, ");
+    position++; 
+    }
+}
+
+int[] array = new int[8];
+FillArray(array);
+FillPrint(array);
