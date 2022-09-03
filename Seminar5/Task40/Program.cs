fca9d1878713d56c49423b264f805274fabb43c6
Tyamenko Vyacheslav <slavo90@mail.ru>
2022-09-03 12:49:11 +0700

    Start seminar 5

---
+++ Seminar5/Task40/Program.cs
@@ -0,0 +1,29 @@
+﻿int userNumberOne = new int();
+int userNumberTwo = new int();
+int userNumberThree = new int();
+
+Console.WriteLine("Введите первое число: ");
+userNumberOne = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число: ");
+userNumberTwo = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите третье число: ");
+userNumberThree = Convert.ToInt32(Console.ReadLine());
+
+string GetTriangle(int a, int b, int c)
+{
+string result = string.Empty;
+if (a < b + c && b < a + c && c < a + b){
+    result = "Треугольник может существовать!";
+}
+else{
+    result = "Не треугольник!";
+}
+return result;
+}
+
+GetTriangle(userNumberOne, userNumberTwo, userNumberThree);
+string Check;
+Check = GetTriangle(userNumberOne, userNumberTwo, userNumberThree);
+Console.WriteLine(Check);
