b00b05e360d4f61f6f49bafad97ea7cdbf98f9a8
Tyamenko Vyacheslav <slavo90@mail.ru>
2022-09-05 23:39:33 +0700

    Add task49 seminar7

---
+++ Seminar7/Task49/Program.cs
@@ -0,0 +1,53 @@
+﻿// Задача №49 Задайте двумерный массив m на n. Найдите элементы, 
+// у которых оба индекса чётные,
+// и замените эти элементы на их квадраты.
+Console.Clear();
+Console.Write("Введите количество строк массива: ");
+int rows = int.Parse(Console.ReadLine());
+
+Console.Write("Введите количество столбцов массива: ");
+int columns = int.Parse(Console.ReadLine());
+int[,] array = GetArray(rows,columns, 0,20);
+PrintArray(array);
+Console.WriteLine();
+PrintArray(ChangeArray(array));
+
+int[,] GetArray(int m, int n, int min, int max)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(min, max);
+        }
+    }
+    return result;
+}
+
+int[,] ChangeArray(int[,] array)
+{
+    for(int i=0;i<array.GetLength(0); i++)
+    {
+        for(int j=0; j<array.GetLength(1);j++)
+        {
+            if(i%2==0 && j%2==0)
+            {
+                array[i,j] *= array[i,j];
+            }
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,] array)
+{
+    for(int i=0;i<array.GetLength(0); i++)
+    {
+        for(int j=0; j<array.GetLength(1);j++)
+        {
+            Console.Write($"{array[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
